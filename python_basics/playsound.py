49ffd02a421e8bb71e2f7867088739ae63769ed8
Nino Saglia <nino.saglia97@gmail.com>
2020-09-17 09:50:06 +0200

    python playsound speelt nu meerdere geluiden af :)))))

--- python_basics/playsound.py
+++ python_basics/playsound.py
@@ -1,6 +1,19 @@
+#/mnt/c/users/ninos/documents/csd2/python_basics
 import os
-os.path.join("C:","Path")
-import simpleaudio.functionchecks as fc
+import simpleaudio as sa
+import time as t
 
-fc.LeftRightCheck.run()
+
+wave_obj = sa.WaveObject.from_wave_file(os.path.join(".","Clap.wav"))
+
+
+#print("Hoeveel keer wil je de clap afspelen?")
+
+x =int(input("Hoeveel keer wil je de clap afspelen?:"))
+
+
+for i in range(0,x):
+    play_obj = wave_obj.play()
+    play_obj.wait_done()
+
 
